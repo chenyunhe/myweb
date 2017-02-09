16b8fd8e532edf257bc0e777757eb4a897b4cc5d
chenyunhe <3177856456@qq.com>
2017-02-09 09:46:58 +0800

    Create 排序.php

---
+++ 排序.php
@@ -0,0 +1,23 @@
+2.php 查出来或模拟的数据
+$data[] = array('volume' => 67, 'edition' => 2);
+$data[] = array('volume' => 86, 'edition' => 1);
+$data[] = array('volume' => 85, 'edition' => 6);
+$data[] = array('volume' => 98, 'edition' => 2);
+$data[] = array('volume' => 86, 'edition' => 6);
+$data[] = array('volume' => 67, 'edition' => 7);
+
+
+3.进行排序
+
+1
+2
+3
+4
+5
+6
+foreach ($data as $key => $row) {
+    $volume[$key]  = $row['volume'];
+    $edition[$key] = $row['edition'];
+}
+//将数据根据 volume 降序排列，根据 edition 升序排列<br>//把 $data 作为最后一个参数，以通用键排序
+array_multisort($volume, SORT_DESC, $edition, SORT_ASC, $data);<br>print_r($data);
